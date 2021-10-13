f4290e1b5637ed7799442c2401e38d8b64092baf
robertomartinez <bobbyjm@me.com>
2021-10-13 15:44:51 -0400

    Updating DentalRecords

--- src/DentalRecords.java
+++ src/DentalRecords.java
@@ -4,29 +4,47 @@ import java.util.Scanner;
 public class DentalRecords {
     private static final Scanner keyboard = new Scanner(System.in);
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int numPeople;
+        char menuOption = 'P';
         char[][][] dentalRecord = new char[5][2][10];
         String[] names = new String[5];
 
         welcomeMessage();
         numPeople = getPeople();
-        //Change this to a loop, where I go to two methods one for names and the other for teeth
-        dentalRecord = inputData(numPeople, names);
-        //Here I make a menu option and also a switch to choose the next method
+        for (int i = 0; i < numPeople; i++) {
+            System.out.print("Please enter the name for family member " + (i + 1) + "  : ");
+            names[i] = keyboard.next();
+            dentalRecord = inputData(dentalRecord, names, i);
+        }
+
+        //Create a Method for the menu option (message, getting input, making sure their input matches)
+        //This switch reads the menu option then runs the respective method
+        switch (menuOption) {
+            case 'P':
+                break;
+            case 'E':
+                break;
+            case 'R':
+                break;
+            case 'X':
+                System.out.print("Exiting the Floridian Tooth Records :-)");
+                break;
+            default:
+                System.out.print("Exiting the Floridian Tooth Records :-)");
+        }
 
 
         //This is just for testing purposes
-        for(int i = 0; i < numPeople; i++) {
+        for (int i = 0; i < numPeople; i++) {
             for (int j = 0; j < 2; j++) {
-                for(int k = 0; k < 10; k++){
+                for (int k = 0; k < 10; k++) {
                     System.out.print(dentalRecord[i][j][k]);
                 }
             }
         }
-
-
     }
+
 
     // Welcome Message
     private static void welcomeMessage() {
@@ -34,7 +52,7 @@ public class DentalRecords {
         System.out.println("--------------------------------------");
     }
 
-    //Getting all the  inputs: number of people, their names, and their teeth
+    //Getting how many people
     private static int getPeople(){
         int numPeople ;
 
@@ -50,47 +68,50 @@ public class DentalRecords {
         return numPeople;
     }
 
-    private static char[][][] inputData(int numPeople, String[] names) {
-        char[][][] dentalRecord = new char[5][2][10];
+    //Gets the string of teeth
+    private static char[][][] inputData(char[][][] dentalRecord, String[] names, int i) {
         String teeth;
 
-        //Spruce this up with improvements found in experiment class
-        for(int i = 0; i < numPeople; i++) {
-            System.out.print("Please enter the name for family member     : ");
-            names[i] = keyboard.next();
-            for (int j = 0; j < 2; j++) {
-                if (j == 0) {
-                    System.out.print("Please enter the uppers for " + names[i] + ": ");
+        for (int j = 0; j < 2; j++) {
+            if (j == 0) {
+                System.out.print("Please enter the uppers for " + names[i] + "     : ");
+                teeth = keyboard.next().toUpperCase(Locale.ROOT);
+                while(!validTeeth(teeth)){
+                    System.out.print("try again              : ");
                     teeth = keyboard.next().toUpperCase(Locale.ROOT);
-                    while(!validTeeth(teeth)){
-                        System.out.print("Try again: ");
-                        teeth = keyboard.next().toUpperCase(Locale.ROOT);
-                    }
-                    for (int k = 0; k < teeth.length(); k++) {
-                        dentalRecord[i][j][k] = teeth.charAt(k);
-                    }
                 }
-                else{
-                    System.out.print("Please enter the lowers for " + names[i] + ": ");
-                    teeth = keyboard.next();
-                    for(int k = 0; k < teeth.length(); k++){
-                        dentalRecord[i][j][k] = teeth.charAt(k);
-                    }
+                for (int k = 0; k < teeth.length(); k++) {
+                    dentalRecord[i][j][k] = teeth.charAt(k);
+                }
+            }
+            else{
+                System.out.print("Please enter the lowers for " + names[i] + "     : ");
+                teeth = keyboard.next().toUpperCase(Locale.ROOT);
+                while(!validTeeth(teeth)){
+                    System.out.print("try again              : ");
+                    teeth = keyboard.next().toUpperCase(Locale.ROOT);
+                }
+                for(int k = 0; k < teeth.length(); k++){
+                    dentalRecord[i][j][k] = teeth.charAt(k);
                 }
             }
         }
+
+
         return dentalRecord;
     }
+
+    //This checks to make sure the input for string of teeth makes sense
     private static boolean validTeeth(String teeth) {
 
         if (teeth.length() > 10) {
-            System.out.print("Too much teeth, ");
+            System.out.print("Too many teeth, ");
             return false;
         }
         else {
             for (int i = 0; i < teeth.length(); i++) {
                 if (teeth.charAt(i) != 'B' && teeth.charAt(i) != 'C' && teeth.charAt(i) != 'M') {
-                    System.out.print("Invalid Teeth, ");
+                    System.out.print("Invalid teeth types, ");
                     return false;
                 }
             }
@@ -98,5 +119,5 @@ public class DentalRecords {
         }
     }
 }
-}
+
 
